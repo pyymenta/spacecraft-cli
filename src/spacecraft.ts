05a394898322e8a17de0d5070d978dad49184ce5
IJustDev <alexanderpanovi@yahoo.de>
2020-10-03 00:43:29 +0200

    Update spacecraft; #7

--- src/spacecraft.ts
+++ src/spacecraft.ts
@@ -1,5 +1,22 @@
 export default class Spacecraft {
-  createMessage(): string {
-    return 'Starter point Spacecraft CLI';
-  }
+
+    public x: number = 0;
+    public y: number = 0;
+
+    public constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public get X() {
+        return this.x;
+    }
+
+    public get Y() {
+        return this.y;
+    }
+
+    createMessage(): string {
+        return 'Starter point Spacecraft CLI';
+    }
 }
