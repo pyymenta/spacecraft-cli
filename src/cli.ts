7032e67dfd013feb833292ac1abd203b86deda8b
Daniele De Martino <danieledemartino.72004@gmail.com>
2020-10-03 15:44:22 +0200

    CLI class to make all classes interact
    This CLI class acts as a glue between the other classes (KeyListener and Spacecraft) to make the Spacecraft interact on keypress.
    The CLI just creates a KeyListener and binds movement events, but also logs the first messages of spacecraft launch.
    I created the basic events (W, A, S, D) and bound them to the spacecraft movement, but I also created a "close" event (with Ctrl+C or Ctrl+X) which can close the program.

--- src/cli.ts
+++ src/cli.ts
@@ -1,23 +1,82 @@
-import * as readline from 'readline';
+import KeyListener from './keyListener';
+import Spacecraft from './spacecraft';
 
-import SpaceCraft from './spacecraft';
+export default class CLI {
+    private input: NodeJS.ReadStream;
+    private output: NodeJS.WriteStream;
+    private keyListener: KeyListener;
+    private spacecraft: Spacecraft;
+    private firstEventCalled: boolean;
+    public latestKeyPressed: string;
 
-const spacecraft = new SpaceCraft(0, 0);
+    public constructor(input: NodeJS.ReadStream, output:  NodeJS.WriteStream, spacecraft: Spacecraft) {
+        this.input = input;
+        this.output = output;
+        this.keyListener = new KeyListener(this.input);
+        this.spacecraft = spacecraft;
 
-let message: string;
+        this.bindKeyEvents();
+    }
 
-message = spacecraft.createMessage();
+    bindKeyEvents(): void {
+        const communicateFirstMovement = () => {
+            if (!this.firstEventCalled) {
+                this.firstEventCalled = true;
+                this.launch();
+            }
+        }
 
-let running = false;
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
-const loop = () => {
-    while (running) {
-        console.log(`X: ${spacecraft.X} Y: ${spacecraft.Y}`);
+        const accelerateEvent = this.keyListener.bindEvent({
+            keyName: "w",
+            handler: (keyName) => {
+                this.latestKeyPressed = keyName;
+                communicateFirstMovement();
+                this.spacecraft.moveForward(1);
+            },
+        });
+
+        const decelerateEvent = this.keyListener.bindEvent({
+            keyName: "s",
+            handler: (keyName) => {
+                this.latestKeyPressed = keyName;
+                communicateFirstMovement();
+                this.spacecraft.moveForward(-1);
+            }
+        });
+
+        const leftMovementEvent = this.keyListener.bindEvent({
+            keyName: "a",
+            handler: (keyName) => {
+                this.latestKeyPressed = keyName;
+                communicateFirstMovement();
+                this.spacecraft.moveForward(0, -1);
+            }
+        });
+
+        const rightMovementEvent = this.keyListener.bindEvent({
+            keyName: "d",
+            handler: (keyName) => {
+                this.latestKeyPressed = keyName;
+                communicateFirstMovement();
+                this.spacecraft.moveForward(0, 1);
+            }
+        });
+
+        const closeEvent = this.keyListener.bindEvent({
+            keyName: ["c", "x"],
+            ctrl: true,
+            handler: function(keyName) {
+                console.log("Closing! Goodbye.");
+                process.exit(1);
+            }
+        });
+    }
+
+    start(): void {
+        console.log('(0, 0) ready for launch');
+    }
+
+    launch(): void {
+        console.log(`(${this.spacecraft.x}, ${this.spacecraft.y}) spacecraft launched from Earth`);
     }
 }
-
-
-console.log(message);
