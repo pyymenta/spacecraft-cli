7d6a0d4c73c68b289b4eeae77a4ae4d510e2374e
Daniele De Martino <danieledemartino.72004@gmail.com>
2020-10-03 15:39:24 +0200

    Integrated Spacecraft class
    I changed the Spacecraft class to have a starting speed value, which is 0, and then, when launched, go to 1.
    Also, I created the moveForward function which manages to move the ship in any sense (including side movement).
    The Spacecraft now also knows about his starting point and his ending point as an ObjectLocation, interface related to locations.ts.

--- src/spacecraft.ts
+++ src/spacecraft.ts
@@ -1,22 +1,45 @@
-export default class Spacecraft {
+import { ObjectLocation } from './locations';
 
+export default class Spacecraft {
+    public start: ObjectLocation;
+    public end: ObjectLocation;
     public x: number = 0;
     public y: number = 0;
+    public speed: number = 0;
+    public launched: boolean;
 
-    public constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
+    public constructor(start: ObjectLocation, end: ObjectLocation) {
+        this.start = start;
+        this.end = end;
+
+        this.x = this.start.x;
+        this.y = this.start.y;
+
+        this.prepareLaunch();
     }
 
-    public get X() {
-        return this.x;
+    private prepareLaunch(): void {
+        this.speed = 1;
     }
 
-    public get Y() {
-        return this.y;
+    public launch(): boolean {
+        return this.launched = true;
     }
 
-    createMessage(): string {
-        return 'Starter point Spacecraft CLI';
+    /* addSpeed can be negative to decrease it; sideMovement can be negative to go to left, else positive to go to right */
+    public moveForward(alterSpeed: number = 0, sideMovement: number = 0) {
+        /* The speed SHOULD NOT change in case of side movement, but only in case of acceleration or deceleration. */
+
+        /* Increases or decreases x axys (left or right movement) */
+        if (sideMovement !== 0) this.x += sideMovement;
+        /* Increases or decreases speed */
+        else if (alterSpeed !== 0) {
+            if (((this.speed + alterSpeed) <= 5) && ((this.speed + alterSpeed) > 0)) {
+                this.speed += alterSpeed;
+            }
+        }
+
+        /* The spacecraft should move forward in any case, even when doing a side movement. */
+        this.y += this.speed;
     }
 }
