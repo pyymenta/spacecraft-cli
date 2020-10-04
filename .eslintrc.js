bd7e352039430ec5560242230f5be6f2ee94c13f
Mounica Chanda <>
2020-10-03 19:26:56 -0500

    Remove jsx support

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
   },
   extends: [
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
