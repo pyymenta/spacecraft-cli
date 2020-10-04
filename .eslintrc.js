edcfcad812731425ca4894c5bb3bee34b6d001f0
Mounica Chanda <>
2020-10-03 19:16:20 -0500

    Remove eslint react plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,13 +7,7 @@ module.exports = {
       jsx: true // Allows for the parsing of JSX
     }
   },
-  settings: {
-    react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
-  },
   extends: [
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
