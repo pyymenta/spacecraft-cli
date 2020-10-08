6ea6b9d908bb09ccff2151e5d6f56d09df0c88c7
CodeMartian <jeffrey.martin@thoughtworks.com>
2020-10-07 22:04:16 -0600

    refactor SpacecraftEmitter to use init function
    The logic in the constructor has been extracted into a new private
    function called init()

--- src/spacecraftEmitter.ts
+++ src/spacecraftEmitter.ts
@@ -3,7 +3,12 @@ import { event_messages } from './events_messages.json';
 
 export default class SpacecraftEmitter {
 	private eventEmitter: EventEmitter = new EventEmitter();
+	
 	constructor(output: NodeJS.WriteStream) {
+		this.init(output);
+	}
+
+	private init(output: NodeJS.WriteStream) {
 		event_messages.forEach((message) => {
 			this.eventEmitter.on(message.id, () => {
 				output.write(` ${message.message} `);
@@ -11,7 +16,7 @@ export default class SpacecraftEmitter {
 		});
 	}
 
-	report(event: string) {
+	public report(event: string) {
 		const event_message = event_messages.find((message) => {
 			return message.id === event;
 		});
