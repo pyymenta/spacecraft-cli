4d6a60599a58557112919e2ca3ad591954d0e046
Lucas <lucas.pimenta94@outlook.com>
2020-10-12 19:41:47 -0300

    fix lint issues

--- src/cli.ts
+++ src/cli.ts
@@ -4,95 +4,88 @@ import events from './events_messages.json';
 import Rocket from './rocket';
 
 export default class CLI {
-	private input: NodeJS.ReadStream;
-	private output: NodeJS.WriteStream;
-	private keyListener: KeyListener;
-	private spacecraft: Spacecraft;
-	private firstEventCalled: boolean;
-	public latestKeyPressed: string;
+    private input: NodeJS.ReadStream;
+    private output: NodeJS.WriteStream;
+    private keyListener: KeyListener;
+    private spacecraft: Spacecraft;
+    private firstEventCalled: boolean;
+    public latestKeyPressed: string;
 
-	public constructor(
-		input: NodeJS.ReadStream,
-		output: NodeJS.WriteStream,
-		spacecraft: Spacecraft
-	) {
-		this.input = input;
-		this.output = output;
-		this.keyListener = new KeyListener(this.input);
-		this.spacecraft = spacecraft;
+    public constructor(input: NodeJS.ReadStream, output: NodeJS.WriteStream, spacecraft: Spacecraft) {
+        this.input = input;
+        this.output = output;
+        this.keyListener = new KeyListener(this.input);
+        this.spacecraft = spacecraft;
 
-		this.bindKeyEvents();
-	}
+        this.bindKeyEvents();
+    }
 
+    bindKeyEvents(): void {
+        const communicateFirstMovement = () => {
+            if (!this.firstEventCalled) {
+                this.firstEventCalled = true;
+                this.launch();
+            }
+        };
 
-	bindKeyEvents(): void {
-		const communicateFirstMovement = () => {
-			if (!this.firstEventCalled) {
-				this.firstEventCalled = true;
-				this.launch();
-			}
-		};
+        const accelerateEvent = this.keyListener.bindEvent({
+            keyName: 'w',
+            handler: (keyName) => {
+                this.latestKeyPressed = keyName;
+                communicateFirstMovement();
+                this.spacecraft.moveForward(1);
+            },
+        });
 
-		const accelerateEvent = this.keyListener.bindEvent({
-			keyName: 'w',
-			handler: (keyName) => {
-				this.latestKeyPressed = keyName;
-				communicateFirstMovement();
-				this.spacecraft.moveForward(1);
-			}
-		});
+        const decelerateEvent = this.keyListener.bindEvent({
+            keyName: 's',
+            handler: (keyName) => {
+                this.latestKeyPressed = keyName;
+                communicateFirstMovement();
+                this.spacecraft.moveForward(-1);
+            },
+        });
 
-		const decelerateEvent = this.keyListener.bindEvent({
-			keyName: 's',
-			handler: (keyName) => {
-				this.latestKeyPressed = keyName;
-				communicateFirstMovement();
-				this.spacecraft.moveForward(-1);
-			}
-		});
+        const leftMovementEvent = this.keyListener.bindEvent({
+            keyName: 'a',
+            handler: (keyName) => {
+                this.latestKeyPressed = keyName;
+                communicateFirstMovement();
+                this.spacecraft.moveForward(0, -1);
+            },
+        });
 
-		const leftMovementEvent = this.keyListener.bindEvent({
-			keyName: 'a',
-			handler: (keyName) => {
-				this.latestKeyPressed = keyName;
-				communicateFirstMovement();
-				this.spacecraft.moveForward(0, -1);
-			}
-		});
+        const rightMovementEvent = this.keyListener.bindEvent({
+            keyName: 'd',
+            handler: (keyName) => {
+                this.latestKeyPressed = keyName;
+                communicateFirstMovement();
+                this.spacecraft.moveForward(0, 1);
+            },
+        });
 
-		const rightMovementEvent = this.keyListener.bindEvent({
-			keyName: 'd',
-			handler: (keyName) => {
-				this.latestKeyPressed = keyName;
-				communicateFirstMovement();
-				this.spacecraft.moveForward(0, 1);
-			}
-		});
+        const closeEvent = this.keyListener.bindEvent({
+            keyName: ['c', 'x'],
+            ctrl: true,
+            handler: function (keyName) {
+                console.log('Closing! Goodbye.\n');
+                process.exit(1);
+            },
+        });
+    }
 
-		const closeEvent = this.keyListener.bindEvent({
-			keyName: ['c', 'x'],
-			ctrl: true,
-			handler: function (keyName) {
-				console.log('Closing! Goodbye.\n');
-				process.exit(1);
-			}
-		});
-	}
-
-	start(): void {
+    start(): void {
         this.output.write(Rocket);
         this.output.write('(0, 0) ready for launch\n');
-	}
+    }
 
-	launch(): void {
-		this.output.write(
-			`(${this.spacecraft.x}, ${this.spacecraft.y}) spacecraft launched from Earth\n`
-		);
-	}
+    launch(): void {
+        this.output.write(`(${this.spacecraft.x}, ${this.spacecraft.y}) spacecraft launched from Earth\n`);
+    }
 
-	exit(): void {
-		this.output.write('Closing! Goodbye.');
+    exit(): void {
+        this.output.write('Closing! Goodbye.');
 
-		this.input.destroy();
-	}
+        this.input.destroy();
+    }
 }
